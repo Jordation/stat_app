f3c1170bf29c142c8ec3406ffa2d3868109ca60b
Jordation <jordanfrish98@gmail.com>
2022-12-10 15:14:15 +1100

    -CRA +next.js

--- frontend_OLD/src/components/ui/GeneralPurpose.js
+++
@@ -1,7 +0,0 @@
-
-import React, { useState } from 'react';
-
-function EmptyDiv(props){
-
-    return (<div style={{maxWidth: props.w, maxHeight: props.h}}></div>)
-} // prob not good to use this long term

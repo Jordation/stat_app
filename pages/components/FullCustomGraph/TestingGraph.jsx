62c731cbccbd0a9b79957f6cd1170d569c2df9e7
Jordation <jordanfrish98@gmail.com>
2023-01-09 15:54:34 +1100

    latest

--- pages/components/FullCustomGraph/TestingGraph.jsx
+++ pages/components/FullCustomGraph/TestingGraph.jsx
@@ -1,19 +1,11 @@
 import React, { useRef, useState } from "react"
 import { set, useForm } from "react-hook-form";
-import Graph from "../graphing/Graph";
-import ComparativeQuerey from "../graphing/ComparativeQuerey";
-import RadarGraph from "../graphing/RadarGraph";
-
-
-
-
-
-
+import BarGraphComponent from "./BarGraphComponent"
 
 const TestingGraph = ({ data, options, plugins}) => {
 
 return (
-        <Graph data={data} options={options} plugins={plugins}/>
+        <BarGraphComponent data={data} options={options} plugins={plugins}/>
     )
 }
 
